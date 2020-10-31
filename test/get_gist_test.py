4eea05083f2a61c06d885434271afb33eaff9c34
tfranckiewicz <tomasz.franckiewicz@blueservices.pl>
2020-10-31 23:01:34 +0100

    Renamed variable in `get_gist_test`

--- test/get_gist_test.py
+++ test/get_gist_test.py
@@ -3,10 +3,10 @@ from main.git_hub_client import GitHubClient
 
 
 class MyTestCase(unittest.TestCase):
-    gitHubClient = GitHubClient("https://api.github.com")
+    git_hub_client = GitHubClient("https://api.github.com")
 
     def test_get_gist(self):
         gist_id = "ee9cd5361077e2c0be784554ba7045fc"
 
-        gist = self.gitHubClient.get_gist(gist_id)
+        gist = self.git_hub_client.get_gist(gist_id)
         self.assertTrue(self, gist["id"] == gist_id)
